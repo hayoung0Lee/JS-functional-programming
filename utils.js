ce99426d63e8b7470e31eb84cdba7c70c9358ccb
hayoung0Lee <hayoung0.lee@gmail.com>
2021-01-26 10:17:38 +0900

    map, filter, reduce 따라하기

--- utils.js
+++ utils.js
@@ -1,10 +1,39 @@
 const drawConsole = (className) => {
-    const element = document.getElementsByClassName(className);
-    return function draw() {
-        Array.prototype.slice.call(arguments).forEach((arg) => {
-            const p = document.createElement('p');
-            p.innerHTML = arg
-            element[0].appendChild(p);
-        })
-    }
-}
+  const element = document.getElementsByClassName(className);
+  return function draw() {
+    Array.prototype.slice.call(arguments).forEach((arg) => {
+      const p = document.createElement("p");
+      p.innerHTML = arg;
+      element[0].appendChild(p);
+    });
+  };
+};
+
+const map = (f, iter) => {
+  let res = [];
+  for (const a of iter) {
+    res.push(f(a));
+  }
+  return res;
+};
+
+const filter = (fn, iter) => {
+  let res = [];
+  for (const a of iter) {
+    if (fn(a)) res.push(a);
+  }
+  return res;
+};
+
+const reduce = (fn, acc, iter) => {
+  if (!iter) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+  for (const n of iter) {
+    acc = fn(acc, n);
+  }
+  return acc;
+};
+
+console.log("loaded");
