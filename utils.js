5c6c39ddfb5c11db14645b77be89751c1cf2692c
hayoung0Lee <hayoung0.lee@gmail.com>
2021-02-11 21:08:48 +0900

    비동기: 동시성 프로그래밍 2따라하기

--- utils.js
+++ utils.js
@@ -24,12 +24,24 @@ const convertPromise = (a, f) => (a instanceof Promise ? a.then(f) : f(a));
 
 // log(add(add(add(add(add(0, 1, 1), 2), 3), 4), 5)); 이렇게 되길 기대하는 함수
 // curry를 적용해서 실행할 함수를 만들었고, fn, acc, iter라는 인자를 나중에 넘겨줘야한다.
+const reduceF = (acc, a, f) => {
+  // then 의 두번째 인자로 reject하는 경우를 처리할 수 있다.
+  return a instanceof Promise
+    ? a.then(
+        (a) => f(acc, a),
+        (e) => (e === nop ? acc : Promise.reject(e))
+      )
+    : f(acc, a);
+};
+
+const head = (iter) =>
+  convertPromise(take(1, iter), ([h]) => {
+    return h;
+  });
+
 const reduce = curry((fn, acc, iter) => {
   if (!iter) {
-    iter = acc[Symbol.iterator]();
-    acc = iter.next().value; // 여기서 첫번째인자가 promise로 들어오는 경우도 있다
-  } else {
-    iter = iter[Symbol.iterator]();
+    return reduce(fn, head((iter = acc[Symbol.iterator]())), iter);
   }
   // for (const n of iter) {
   // let cur;
@@ -39,12 +51,16 @@ const reduce = curry((fn, acc, iter) => {
   //   // 중간에 promise가 들어오면 계속 연속적으로 promise체인에 걸린다
   //   // 이렇게 되면 불필요한 load가 생길 수 있다
   // }
-
+  iter = iter[Symbol.iterator]();
   return convertPromise(acc, function recur(acc) {
     let cur;
     while (!(cur = iter.next()).done) {
-      const n = cur.value;
-      acc = fn(acc, n);
+      // 여기서 꺼낸애는 promise인 상태로 있어버린다.
+      // const n = cur.value;
+      // acc = fn(acc, n);
+      // nop을 풀고, promise 를 풀ㅇㅓ내는걸 함수로 따로 뗀다 ->  문제 정리가 쉬워진다
+      acc = reduceF(acc, cur.value, fn);
+      // 첫번째와 promise가 전달된다.
       if (acc instanceof Promise) return acc.then(recur);
     }
     return acc;
@@ -74,14 +90,26 @@ const take = curry((l, iter) => {
   let res = [];
   // for (const a of iter) {
   iter = iter[Symbol.iterator]();
-  let cur;
-  while (!(cur = iter.next()).done) {
-    const a = cur.value;
-    res.push(a);
-    if (res.length === l) return res;
-  }
 
-  return res;
+  return (function recur() {
+    let cur;
+    while (!(cur = iter.next()).done) {
+      const a = cur.value;
+      // a가 promise인 경우가 있다.
+      if (a instanceof Promise) {
+        // promise를 반환한다
+        return (
+          a
+            .then((a) => ((res.push(a), res).length == l ? res : recur()))
+            // nop인 경우에는 그냥 넘어가고, 이거말고 다른 에러면 reject를 띄운다
+            .catch((err) => (err === nop ? recur() : Promise.reject(err)))
+        );
+      }
+      res.push(a);
+      if (res.length === l) return res;
+    }
+    return res;
+  })();
 });
 
 // 범위만큼의 숫자를출력
@@ -118,12 +146,21 @@ L.range = function* (l) {
 };
 
 L.map = curry(function* (f, iter) {
-  for (const a of iter) yield f(a);
+  for (const a of iter) {
+    yield convertPromise(a, f);
+  }
 });
+
+const nop = Symbol("nop");
 
 L.filter = curry(function* (f, iter) {
   for (const a of iter) {
-    if (f(a)) yield a;
+    const b = convertPromise(a, f); // then은 다시 promise를 반환한다.
+    // log(b); // b가 promise로 오면 무조건 promise라 통과되는거고 아직 값이 반영이 안된다
+    // then을 통해서 값을 풀어낸다. 아니면 안흘러가도록 kleslie 컴포지션을 활용한다
+    if (b instanceof Promise)
+      yield b.then((b) => (b ? a : Promise.reject(nop)));
+    else if (b) yield a;
   }
 });
 
@@ -200,3 +237,31 @@ const map = curry(pipe(L.map, takeAll));
 
 // filter를 L.filter로 바꾸기
 const filter = curry(pipe(L.filter, takeAll));
+
+// 지연된 함수열을 병렬적으로 평가하기
+// Concurrency
+// 비동기가 일어나느걸 기다리지않고, 다 실행해버린다
+function noop() {}
+const catchNoop = (arr) => (
+  arr.forEach((a) => (a instanceof Promise ? a.catch(noop) : a)), arr
+);
+
+const C = {};
+C.reduce = curry((f, acc, iter) => {
+  // console.log("creduce", [...acc]);
+  // reject이 된후에 catch가 되면 error자체는 잘 catch가 되지만, error로그는 찍혀버린다.
+  // const iter2 = catchNoop(iter ? [...iter] : [...acc]);
+  // catch를한 promise를 전달하는 것이 아니라, 일단 여기서 임시로 catch를 해서 error가 나지 않도록 하는것이다.
+  // 이렇게 미리 catch를 달아놔서 같은 콜스택에서 catch만 처리하고,실제로는 catch는 붙어있지않은 promise를 전달
+  return iter
+    ? reduce(f, acc, catchNoop([...iter]))
+    : reduce(f, catchNoop([...acc]));
+});
+
+C.take = curry((l, iter) => take(l, catchNoop([...iter])));
+
+C.takeAll = C.take(Infinity);
+
+C.map = curry(pipe(L.map, C.takeAll));
+
+C.filter = curry(pipe(L.filter, C.takeAll));
